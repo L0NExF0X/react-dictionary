3b19474e054508b241d7d691f98b0b0c70a52af6
Katie Perez <perezkatie94@gmail.com>
2021-07-01 22:22:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,29 +10,29 @@ export default function App() {
           <h1 className="">Dictionary App</h1>
         </header>
         <Dictionary />
-        <footer className="App-footer">
-          <p>
-            This app was coded by{" "}
-            <a
-              href="https://www.linkedin.com/in/katie-p%C3%A9rez-0a8655209/"
-              target="_blank"
-              rel="noreferrer"
-              title="LinkedIn Profile - Katie Pérez"
-            >
-              Katie Pérez
-            </a>{" "}
-            and is{" "}
-            <a
-              href="https://github.com/L0NExF0X/react-dictionary"
-              target="_blank"
-              rel="noreferrer"
-              title="GitHub Repository"
-            >
-              open-sourced on GitHub
-            </a>
-          </p>
-        </footer>
       </div>
+      <footer className="App-footer mt-5">
+        <p>
+          This app was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/katie-p%C3%A9rez-0a8655209/"
+            target="_blank"
+            rel="noreferrer"
+            title="LinkedIn Profile - Katie Pérez"
+          >
+            Katie Pérez
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/L0NExF0X/react-dictionary"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub Repository"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
